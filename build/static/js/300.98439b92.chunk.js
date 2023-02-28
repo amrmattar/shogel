3f604d9576a4355b9775f5ee19e2ee400fc253fa
ahmed-ashraf-dv <ahmdgun0@gmail.com>
2023-02-28 04:46:58 +0200

    _

--- build/static/js/300.98439b92.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkshoghl_app=self.webpackChunkshoghl_app||[]).push([[300],{45300:function(u,e,n){n.r(e),n.d(e,{default:function(){return a}});n(47313);var r=n(46417),s=(n(22297),n(97890)),t=n(66367),a=function(){(0,t.useMediaQuery)({query:"(max-width: 992px)"}),(0,s.TH)();return(0,r.jsx)("div",{})}}}]);
